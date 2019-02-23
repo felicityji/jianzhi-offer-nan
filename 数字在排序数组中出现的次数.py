e6098a48e3fac36a7ff5305a35eb030f6ef49dcc
Nan <1657141675@qq.com>
2019-02-23 11:19:12 +0800

    Update 数字在排序数组中出现的次数.py

--- 数字在排序数组中出现的次数.py
+++ 数字在排序数组中出现的次数.py
@@ -16,8 +16,6 @@ class Solution:
             return 0
         return self.GetLastK(data, k) - self.GetFirstK(data, k) + 1
         
-
-
     def GetFirstK(self, data, k):
         low = 0
         high = len(data) - 1
@@ -34,7 +32,7 @@ class Solution:
                     high = mid - 1
         return -1
 
-    def GetLastK(self, data, k):
+    def GetLastK(self, data, k): #找到k时即data[mid] = k ，判断当前位置是否时最后一个数字或者后一个是否等于k，若等于k并且当前位置不是最后一个位置，则low=mid+1,目的是在mid（当前）的后一段再重新找k，重复上面的逻辑，直到找到最后一个k为止并返回当前k的位置
         low = 0
         high = len(data) - 1
         while low <= high:
@@ -43,9 +41,9 @@ class Solution:
                 high = mid - 1
             elif data[mid] < k:
                 low = mid + 1
-            else:
-                if mid == high or data[mid + 1] != k:
+            else:   #data[mid] = k 
+                if mid == high or data[mid + 1] != k: #mid = high已经找到最后一个数字时，返回mid。或者在当前mid后一个数字不等于k时，返回mid
                     return mid
-                else:
+                else:   #这里就是 data[mid + 1] = k && mid != high  这时需要去到mid后面一段找k
                     low = mid + 1
         return -1
